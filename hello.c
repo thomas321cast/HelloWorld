40c3ecac07c8518a86348b8f1d609185f414c9df
Thomas iOS <thomas321.ios@gmail.com>
2016-02-17 22:40:23 +1300

    Added function321ios()

--- hello.c
+++ hello.c
@@ -1,10 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void function321ios()
+{
+  printf("321ios");
+}
+
 int main() {
   printf("Hello World, take 5!\n");
   printf("by Thomas Wong\n");
   printf("How about a third line?")
+
   // Add some comments here for testing
+  function321ios();
+  
   return 0;
 }

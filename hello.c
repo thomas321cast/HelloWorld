bfdcd67a3659cd9011d600f7f61353a6d98014f9
Thomas Wong <thomas321@gmail.com>
2016-02-17 22:35:57 +1300

    Added function321()

--- hello.c
+++ hello.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void function321() {
+  printf("321");
+}
+
 int main() {
   printf("Hello World, take 3A!\n");
   printf("by Thomas Wong\n");
   printf("How about a third line?")
+  function321();
+
   // Add some comments here for testing
   return 0;
 }
